602a5a0862e70506c76fe82431dd41a5605714f7
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2019-07-30 13:29:10 +0200

    Updates

---
+++ precache-manifest.8e49dec514338a63f78f4aecd5ea17e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9b355b397b347455fcad8ecc1ad7af",
+    "url": "/wholesome-doggos/index.html"
+  },
+  {
+    "revision": "ab9742d0c9f2029456c9",
+    "url": "/wholesome-doggos/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/wholesome-doggos/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ab9742d0c9f2029456c9",
+    "url": "/wholesome-doggos/static/js/main.29178277.chunk.js"
+  },
+  {
+    "revision": "b2648adbf6bb3bcbfcae",
+    "url": "/wholesome-doggos/static/js/runtime~main.d4be0c9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wholesome-doggos/static/media/logo.5d5d9eef.svg"
+  }
+]);
